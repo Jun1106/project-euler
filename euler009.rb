27bcf71676865036aa68acce50756d90f6a613cb
Junichi Ariyoshi <ariyoshi@toydea.com>
2017-12-04 17:18:21 +0900

    Problem 9

---
+++ euler009.rb
@@ -0,0 +1,32 @@
+#! ruby -E Windows-31J:utf-8
+
+def euler009
+  a = 1
+  b = 0
+  c = 0
+
+  # 処理
+  found = false
+  loop do
+    b = a + 1
+    
+    while b < 1000 - a - (a + 2)
+      c = 1000 - (a + b)
+
+      if (a * a) + (b * b) == (c * c)
+        found = true
+        break
+      end
+
+      break if (a * a) + (b * b) > (c * c)
+
+      b += 1
+    end
+
+    break if found
+
+    a += 1
+  end
+
+  p "#{a} + #{b} + #{c} = 1000, #{a}^2 + #{b}^2 = #{c}^2"
+end
