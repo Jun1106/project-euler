74f5aa036100947af5c849c5e98e8ca112fb9fb8
Junichi Ariyoshi <ariyoshi@toydea.com>
2017-12-14 20:04:46 +0900

    Problem 20

--- euler020.rb
+++ euler020.rb
@@ -1,7 +1,11 @@
 #! ruby -E Windows-31J:utf-8
 
-def euler016
-    n = (2**1000).to_s
+def euler020
+    n = 1
+    (2..100).each do |v|
+        n *= v
+    end
+    n = n.to_s
 
     x = 0
     (0..n.length).each do |i|
